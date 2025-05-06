d7937a99b941bb155ee300f96bf2de5afb6755ca
nemanjadjurisic011 <138724604+nemanjadjurisic011@users.noreply.github.com>
2025-05-06 14:54:24 +0200

    Handle 404 error and uknown routes

--- src/modules/transactions/routes.tsx
+++ src/modules/transactions/routes.tsx
@@ -1,9 +1,14 @@
 import { RouteObject } from 'react-router-dom';
 import { TransactionPage } from './pages/TransactionPage';
+import { Navigate } from 'react-router-dom';
 
 export const transactionRoutes: RouteObject[] = [
   {
     path: '/transactions',
     element: <TransactionPage />,
+  },
+  {
+    path: '*',
+    element: <Navigate to="/" replace />,
   },
 ]; 
